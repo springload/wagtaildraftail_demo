4baa3dfea084c509bacb7fd6cee1ffcae9979c27
Thibaud Colas <thibaudcolas@gmail.com>
2017-05-16 23:35:45 +0300

    Use heroku settings as default

--- wagtaildraftail_demo/wsgi.py
+++ wagtaildraftail_demo/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtaildraftail_demo.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtaildraftail_demo.settings.heroku")
 
 application = get_wsgi_application()
